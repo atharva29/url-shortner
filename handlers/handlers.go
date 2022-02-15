6c1fb54107e814f040e1f4528b8df06aff8bbafa
atharva29 <atharvaajgaonkar29@gmail.com>
2022-02-15 23:55:36 +0530

    handler added

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,12 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	shortener "github.com/url-shortner/shortner"
+	"github.com/url-shortner/store"
 )
 
 type UrlCreationRequest struct {
@@ -9,7 +14,24 @@ type UrlCreationRequest struct {
 }
 
 func CreateShortUrl(c *gin.Context) {
+	var creationRequest UrlCreationRequest
+	if err := c.ShouldBindJSON(&creationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl)
+	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl)
+
+	host := "http://localhost:8100/"
+	c.JSON(200, gin.H{
+		"message":   "short url created successfully",
+		"short_url": host + shortUrl,
+	})
 }
 
 func HandleShortUrlRedirect(c *gin.Context) {
+	shortUrl := c.Param("shortUrl")
+	initialUrl := store.RetrieveInitialUrl(shortUrl)
+	c.Redirect(302, initialUrl)
 }
