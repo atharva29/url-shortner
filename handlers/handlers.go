be9faed7ab376baf258705199ec2355e6cf39312
atharva29 <atharvaajgaonkar29@gmail.com>
2022-04-14 14:25:56 +0530

    Update handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,7 +8,6 @@ import (
 
 	shortener "github.com/url-shortner/shortner"
 	"github.com/url-shortner/store"
-	"github.com/url-shortner/utils"
 )
 
 // UrlCreationRequest request object for url-shortner
@@ -25,7 +24,7 @@ func appendHTTPSIfNotExists(longURL string) string {
 }
 
 // CreateShortUrl creates short URL from long URL coming from request and stores in text file
-func CreateShortUrl(c *gin.Context) {
+func CreateShortUrl(c *gin.Context, port string) {
 	var creationRequest UrlCreationRequest
 	if err := c.ShouldBindJSON(&creationRequest); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -39,7 +38,7 @@ func CreateShortUrl(c *gin.Context) {
 
 	c.JSON(200, gin.H{
 		"message":   "short url created successfully",
-		"short_url": utils.Host + shortUrl,
+		"short_url": "http://localhost:" + port + "/" + shortUrl,
 	})
 }
 
